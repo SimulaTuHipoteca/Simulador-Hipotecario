647f88f6233dcd9f0dc2bd7b8b8e973d3737305f
Kaoba <hipotecasimulador@gmail.com>
2026-04-10 22:21:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -465,13 +465,6 @@ if (prestamoNecesario === 0 && agregarVivienda) {
   mensajePerfil.innerText = "No necesitas financiación: tus ahorros cubren la operación.";
   mensajePerfil.classList.add("mensaje-ok");
 
-} else if ((edad1 < 35 || edad2 < 35) && faltaDinero > 0) {
-
-  mensajePerfil.innerHTML = `
-    Puedes financiar el <strong>100%</strong> de la vivienda.<br><br>
-    Solo necesitas cubrir los gastos (~${formatMoney(gastos)}).
-  `;
-  mensajePerfil.classList.add("mensaje-warning");
 
 } else if (faltaDinero > 0) {
 
